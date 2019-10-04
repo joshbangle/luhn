a827df2db3614906d656746fa29e5d66b3a5cb38
Joshua Bangle <joshua.bangle@gmail.com>
2019-10-04 19:52:48 +0000

    deleted excess code

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -3,8 +3,6 @@ module Luhn
 
   def self.is_valid?(number)
     hold = number.to_s.split('').map(&:to_i)
-    other = []
-    doubled = []
     i = -2
     l = 2
     while l <= hold.length
@@ -17,15 +15,11 @@ module Luhn
     end
 
     if hold.sum.even?
-
       return true
     else
-
       return false
     end
-
   end
-
-
 end
+
 Luhn.is_valid?(4194560385008504)
